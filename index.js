a92d5453da515777b40e0f0ca54c89b8b4b9f464
luisjosetorres12 <luis.torres784@gmail.com>
2020-12-26 12:47:18 -0500

    Add vision and create Views Folder

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
 const hapi = require('@hapi/hapi')
+const inert = require('inert')
+const path = require('path')
+const handlerbars = require('handlebars')
+const vision = require('vision')
 
 const server = hapi.server({
   port: 3000,
-  host: 'localhost'
+  host: 'localhost',
+  routes: {
+    files: {
+      relativeTo: path.join(__dirname, 'public')
+    }
+  }
 })
 
 const init = async () => {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (req, h) => {
-      console.log('Llego peticion')
-      return { hello: 'world' }
-    }
-  })
-
   try {
+    // Registrando Plugins
+    await server.register(inert)
+    await server.register(vision)
+
+    //Vistas
+    server.views({
+      engines:{
+        hbs: handlerbars
+      },
+      relativeTo: __dirname,
+      path: 'views',
+      layout:true,
+      layoutPath: 'views'
+    })
+
+    // Rutas
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (req, h) => {
+        console.log('Llego peticion')
+        return h.view('index', {title:'Home'})
+      }
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          redirectToSlash:true
+        }
+      }
+    })
+
     await server.start()
   } catch (error) {
     console.log(error)
